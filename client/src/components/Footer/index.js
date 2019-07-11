bb33e009526f7a0978dc9d39eb07546f005270de
dvshannon96@gmail.com <dvshannon96@gmail.com>
2019-07-11 13:33:17 -0400

    refractored everything

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="text-center">
+        Deyon Shannon
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,16 +1,16 @@
-const path = require("path")
+const path = require("path");
 const router = require("express").Router();
 const bookRoutes = require("./books");
 const googleRoutes = require("./google");
 
-
 // Book routes
 router.use("/books", bookRoutes);
 
-//Google routes
+// Google Routes
 router.use("/google", googleRoutes);
 
-router.use(function (req, res) {
+// For anything else, render the html page
+router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 
